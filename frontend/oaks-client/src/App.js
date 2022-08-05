af89d2a458257a67714db3c27ac19f874f33d20d
krisblarq <naturalicon007@gmail.com>
2022-08-05 23:32:46 +0100

    server setup

---
+++ frontend/oaks-client/src/App.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+require("./App.css");
+function App() {
+    return (<h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>);
+}
+exports["default"] = App;
