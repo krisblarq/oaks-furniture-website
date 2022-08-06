832a30b20ff0566bae11b515313a0e6e3f0d4e11
krisblarq <naturalicon007@gmail.com>
2022-08-06 12:36:02 +0100

    Mongodb setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+mongoose
+  .connect(
+    "mongodb+srv://oaks:wisdom0074u@cluster0.sinxrnz.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Database server connected succesfully"))
+  .catch((err: String) => console.log(err));
+
 app.listen(5000, () => {
   console.log("Backend server is active");
 });
