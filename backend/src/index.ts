55d9371d49979d261257d24440d70aa6c05c0e58
krisblarq <naturalicon007@gmail.com>
2022-08-07 22:28:59 +0100

    added controllers and models

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require(""./routes/userRoute")
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ mongoose
   .then(() => console.log("Database server connected succesfully"))
   .catch((err: String) => console.log(err));
 
+app.use("/api/user", userRoute)
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is active");
 });
---
+++ backend/src/routes/usersRoute.ts
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+
+router.get("/user");
+
+module.exports = router;
